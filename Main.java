a0821bad5e2e6866b73d93cbcf462e3a8887e614
Annkoro <96101314+Annkoro@users.noreply.github.com>
2022-06-04 19:53:49 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ class Main {
     System.out.print("何人分の情報を入力しますか：");
     int n = scanner.nextInt();
     int maxAge = 0;
+    int totalAge = 0;
     for (int i = 0; i < n; i++) {
       System.out.println(i + 1 + "人目");
       System.out.print("名前：");
@@ -17,6 +18,7 @@ class Main {
       if (age > maxAge) {
         maxAge = age;
       }
+      totalAge += age;
       System.out.print("身長(m)：");
       double height = scanner.nextDouble();
       System.out.print("体重(kg)：");
@@ -24,5 +26,6 @@ class Main {
       Person.printData(firstName, lastName, age, height, weight);
     }
     System.out.println("最高年齢は" + maxAge + "歳です");
+    System.out.println("平均年齢は" + totalAge / n + "歳です");
   }
 }
